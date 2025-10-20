b1f8b3b9f8ec942b45a33224888bda65f8243a75
haniyeh amiri <amirihaniyhe@gmail.com>
2025-10-20 12:21:34 +0330

    delete an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,19 @@
+'use client'
+
+import { useRouter } from "next/navigation"
 import IconTrash from "./IconTrash"
+import axios from "axios"
 
 const DeleteIssueButton = ({issueId}: {issueId: number}) => {
+  const router = useRouter()
+  const handleDelete = async ()=> {
+   await  axios.delete(`/api/issues/${issueId}`)
+   router.push('/issues')
+   router.refresh()
+  }
+
   return (
-    <button className='text-bold items-center cursor-pointer flex text-red-400 -mt-8 bg-white px-2 py-[2px] -ml-[1.4rem]  rounded-[0.65rem] text-2xl'>
+    <button onClick={handleDelete} className='text-bold items-center cursor-pointer flex text-red-400 -mt-8 bg-white px-2 py-[2px] -ml-[1.4rem]  rounded-[0.65rem] text-2xl'>
       <IconTrash />
     </button>
   )
